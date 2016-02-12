5dac633a2fb7f1bb9922f2ebcd960035623915d2
Jan Hackel <jhunovis+github@googlemail.com>
2016-02-12 18:44:30 +0100

    Add Spock specification for FizzBuzzSequenceJava.
    Spock matches collections for equality in a very nice and simple way.
    No need for Hamcrest matchers for such assertions here.

---
+++ fizz-buzz/src/test/groovy/jhunovis/fizzbuzz/FizzBuzzSequenceSpecification.groovy
@@ -0,0 +1,25 @@
+package jhunovis.fizzbuzz
+
+import spock.lang.Specification
+
+import java.util.stream.IntStream
+
+/**
+ * A Spock specification for {@link FizzBuzzSequenceJava}.
+ *
+ * @author <a href="mailto:jhunovis@gmail.com">Jan Hackel</a>
+ * @version $Revision$ $Date$ $Author$
+ */
+class FizzBuzzSequenceSpecification extends Specification {
+    def "translate number sequences to Fizz-Buzz numbers. Var-arg version."() {
+        expect:
+        new FizzBuzzSequenceJava().translate(1, 2, 3, 4, 5, 6, 10, 12, 15, 16, 20, 45, 89, 90) ==
+                ["1", "2", "Fizz", "4", "Buzz", "Fizz", "Buzz", "Fizz", "FizzBuzz", "16", "Buzz", "FizzBuzz", "89", "FizzBuzz"];
+    }
+
+    def "translate number sequences to Fizz-Buzz numbers. Stream version."() {
+        expect:
+        new FizzBuzzSequenceJava().translate(IntStream.of(1, 2, 3, 4, 5, 6, 10, 12, 15, 16, 20, 45, 89, 90)) ==
+                ["1", "2", "Fizz", "4", "Buzz", "Fizz", "Buzz", "Fizz", "FizzBuzz", "16", "Buzz", "FizzBuzz", "89", "FizzBuzz"];
+    }
+}
