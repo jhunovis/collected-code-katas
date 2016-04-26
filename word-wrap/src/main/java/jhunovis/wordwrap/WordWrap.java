41839c61427a5ed80304f25fef1cc54addeec1fa
Jan Hackel <jhunovis+github@googlemail.com>
2016-04-26 12:35:11 +0200

    First crude attempt on the "word-wrap" kata.
    It uses pure Java without any Java 8 features, i.e. pure procedural style.
    The test is written as a Spock specification and should be reusable
    for other attempt.

---
+++ word-wrap/src/main/java/jhunovis/wordwrap/WordWrap.java
@@ -0,0 +1,78 @@
+package jhunovis.wordwrap;
+
+import com.google.common.base.Splitter;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Implement the word-wrap kata in Java.
+ * <p/>
+ * You write a class called "Wrapper", that has a single function named "wrap"
+ * that takes two arguments, a string, and a column number. The function returns
+ * the string, but with line breaks inserted at just the right places to make
+ * sure that no line is longer than the column number. You try to break lines at word boundaries.
+ * Like a word processor, break the line by replacing the last space in a line with a newline.
+ * (Quoted from <a href="http://codingdojo.org/cgi-bin/index.pl?KataWordWrap">http://codingdojo.org/cgi-bin/index.pl?KataWordWrap</a>)
+ *
+ * @author <a href="mailto:jhunovis@gmail.com">Jan Hackel</a>
+ * @version $Revision$ $Date$ $Author$
+ */
+public final class WordWrap {
+    /**
+     * Split a given text line into multiple text line so that no line is longer than {@code columnWidth}.
+     *
+     * @param textToSplit the text to split
+     * @param columnWidth no line in the result will be longer than this minus one (for the newline).
+     * @return the split text
+     */
+    public List<String> wrap(String textToSplit, final int columnWidth) {
+        List<String> lines = new ArrayList<>();
+        String[] splitAtSpace = textToSplit.split(" ");
+        String currentLine = "";
+        for (String word : splitAtSpace) {
+            String currentLinePlusNextWord = join(currentLine, word);
+            if (currentLinePlusNextWord.length() <= columnWidth) {
+                currentLine = currentLinePlusNextWord;
+            } else {
+                if (StringUtils.isNotBlank(currentLine)) {
+                    lines.add(currentLine + "\n");
+                }
+                lines.addAll(appendNewLine(intraWordSplit(word, columnWidth)));
+                currentLine = fittingTailOf(word, columnWidth);
+            }
+        }
+        if (StringUtils.isNotBlank(currentLine)) {
+            lines.add(currentLine + "\n");
+        }
+        return lines;
+    }
+
+    private String join(String currentLine, String word) {
+        return StringUtils.isBlank(currentLine) ? word : currentLine + " " + word;
+    }
+
+    private Collection<? extends String> appendNewLine(Collection<? extends String> lines) {
+        ArrayList<String> linesWithLineBreak = new ArrayList<>(lines.size());
+        for (String line : lines) {
+            linesWithLineBreak.add(line + "\n");
+        }
+        return linesWithLineBreak;
+    }
+
+    private Collection<? extends String> intraWordSplit(String word, int columnWidth) {
+        List<String> wordSplitAtColumns = Splitter.fixedLength(columnWidth).splitToList(word);
+        if (wordSplitAtColumns.get(wordSplitAtColumns.size() - 1).length() < columnWidth) {
+            return wordSplitAtColumns.subList(0, wordSplitAtColumns.size() - 1);
+        } else {
+            return wordSplitAtColumns;
+        }
+    }
+
+    private String fittingTailOf(String word, int columnWidth) {
+        int tailBegin = word.length() / columnWidth * columnWidth;
+        return word.substring(tailBegin);
+    }
+}
---
+++ word-wrap/src/test/groovy/jhunovis/wordwrap/WordWrapSpec.groovy
@@ -0,0 +1,57 @@
+package jhunovis.wordwrap
+
+import spock.lang.Unroll
+
+/**
+ * @author <a href="mailto:jhunovis@gmail.com">Jan Hackel</a>
+ * @version $Revision$ $Date$ $Author$
+ */
+class WordWrapSpec extends spock.lang.Specification {
+
+    @Unroll("Whitespace splits: split #text at column #column should be #splitText")
+    def "Exact hits at space"() {
+        expect:
+        new WordWrap().wrap(text, column) == splitText
+
+        where:
+        text             | column || splitText
+        ""               | 5      || []
+        "AAAA"           | 4      || ["AAAA\n"]
+        "AAAA BBBB CCCC" | 4      || ["AAAA\n", "BBBB\n", "CCCC\n"]
+    }
+
+    @Unroll("Intra-word splits: split #text at column #column should be #splitText")
+    def "Intra-word splits"() {
+        expect:
+        new WordWrap().wrap(text, column) == splitText
+
+        where:
+        text             | column || splitText
+        "AAAA"           | 2      || ["AA\n", "AA\n"]
+        "AAAA BBBB CCCC" | 2      || ["AA\n", "AA\n", "BB\n", "BB\n", "CC\n", "CC\n"]
+    }
+
+    @Unroll("Real-word splits: split #text at column #column should be #splitText")
+    def "Real-world splits"() {
+        expect:
+        new WordWrap().wrap(text, column) == splitText
+
+        where:
+        text                                                              | column || splitText
+        "No word in this text is longer than the column width."           | 7      || ["No word\n", "in this\n", "text is\n",
+                                                                                       "longer\n", "than\n", "the\n",
+                                                                                       "column\n", "width.\n"]
+        "One extravagant word will not fit." | 9      || ["One\n", "extravaga\n", "nt word\n", "will not\n", "fit.\n"]
+    }
+
+    def "Split lines should end on newline"() {
+        expect:
+        new WordWrap().wrap(text, column).every {it.endsWith("\n")}
+
+        where:
+        text               | column || splitText
+        ""                 | 1      || _
+        "Line with spaces" | 4      || _
+        "AAA BBB CCC DDD"  | 3      || _
+    }
+}
